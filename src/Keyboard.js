22213d64d32fd83a94022840e288c0600a6bd3dc
cragtech <cragtechnology@gmail.com>
2025-12-27 14:39:16 -0700

    Deleted unnecessary files and feature from stock react app. Set up components for app and added them into App.js

---
+++ src/Keyboard.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Keyboard() {
+  return (
+    <div className="Keyboard">
+      <span>Keyboard</span>
+    </div>
+  );
+}
+
+export default Keyboard;
---
+++ src/TitleBar.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function TitleBar() {
+  return (
+    <div className="TitleBar">
+      <span>TitleBar - Hang Man Game</span>
+    </div>
+  );
+}
+
+export default TitleBar;
